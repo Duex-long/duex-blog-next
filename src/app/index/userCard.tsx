26905e83af082c01912dc2e0e3e269df3e0c1d97
Shangqinglong <long.shangqing@gempoll.com>
2023-02-01 14:10:35 +0800

    动画天价

--- src/app/index/userCard.tsx
+++ src/app/index/userCard.tsx
@@ -2,6 +2,9 @@ const UCard = () => {
   return (
     <div className="Ucard">
       <div className="Ucard-avatar"></div>
+      <div className="Ucard-name">
+        <h3>PSN:Ultra_to_me</h3>
+      </div>
     </div>
   );
 };
